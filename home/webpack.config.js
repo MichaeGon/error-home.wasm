9bf8bf9bb67b13e930664875f09158b96b5f0a8f
MichaeGon <lodnix@gmail.com>
2018-09-26 20:45:18 +0900

    fix home dist

--- home/webpack.config.js
+++ home/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "dist/home"),
+    path: path.resolve(__dirname, "dist"),
     filename: "index.js",
   },
   plugins: [
