9ee6c649d9278fbb789848e3eb16e995dce863b7
MichaeGon <lodnix@gmail.com>
2018-09-26 19:08:53 +0900

    fix dist

--- home/webpack.config.js
+++ home/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "home_dist"),
+    path: path.resolve(__dirname, "dist/home"),
     filename: "index.js",
   },
   plugins: [
--- jump/webpack.config.js
+++ jump/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "jump_dist"),
+    path: path.resolve(__dirname, "dist/jump"),
     filename: "index.js",
   },
   plugins: [
